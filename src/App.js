50501f5134435b422380d023f8587fa7aae8d7e6
Neam6009 <neeleshnani99@gmail.com>
2023-10-10 13:54:25 +0530

    completed simple react app

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
 
     return (
         <div className="app">
-            <h1>LalaLand</h1>
+            <h1>Ebomma</h1>
 
             <div className="search">
                 <input
